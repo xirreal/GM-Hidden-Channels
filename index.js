012130f1a4c343b387f7f29d3d0082ea9d4b791e
alethéia <34745171+xirreal@users.noreply.github.com>
2021-08-25 01:23:41 +0200

    async test 3?

--- index.js
+++ index.js
@@ -173,27 +173,22 @@ export default {
 
                 await new Promise(r => {
                     (function wait () {
-                        if(hiddenChannelCache[originalArgs[0]]?.hiddenChannels !== undefined) r;
+                        if(hiddenChannelCache[originalArgs[0]]?.hiddenChannels !== undefined) r();
                         else setTimeout(wait, 0);
-                    })()});
-
-
-                hiddenChannelCache[originalArgs[0]].hiddenChannels.forEach(channel => {
-                    if(!channel) return previousReturn;
-                    const channelsInCategory = previousReturn[channel.parent_id || "null"];
-                    if (channelsInCategory.filter((item) => item?.channel?.id === channel.id).length) return previousReturn;
-                    channelsInCategory.push({ channel: channel, index: 0 });
+                })()}).then(() => {
+                    hiddenChannelCache[originalArgs[0]].hiddenChannels.forEach(channel => {
+                        if(!channel) return previousReturn;
+                        const channelsInCategory = previousReturn[channel.parent_id || "null"];
+                        if (channelsInCategory.filter((item) => item?.channel?.id === channel.id).length) return previousReturn;
+                        channelsInCategory.push({ channel: channel, index: 0 });
+                    });
+    
+                    return previousReturn;
                 });
-
-                return previousReturn;
             });
 
             Unpatch.ChannelItem = patcher.patch(ChannelItem, "default", (originalArgs) => {
                 // originalArgs[0] are the props
-
-                // while(hiddenChannelCache[originalArgs[0].channel.guild_id].channels.length === undefined) {
-                //     console.log("waiting...")
-                // }
 
                 if(!isChannelVisible(originalArgs[0].channel.id)) originalArgs[0]["aria-label"] += " hidden";
                 return originalArgs;
