61a36f28a869690d2f1ed028f4c93201fd6b954c
alethéia <34745171+xirreal@users.noreply.github.com>
2021-08-29 11:52:14 +0200

    Fix order of arguments due to discord change

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const handleChannelChange = data => {
 const isChannelVisible = channelId => {
     const channel = getChannel(channelId);
     if(!channel || !channelId || [ChannelTypes.DM, ChannelTypes.GROUP_DM].includes(channel?.type)) return true;
-    return [ChannelTypes.GUILD_TEXT, ChannelTypes.GUILD_VOICE, ChannelTypes.GUILD_STAGE_VOICE, ChannelTypes.GUILD_ANNOUNCEMENT, ChannelTypes.ANNOUNCEMENT_THREAD, ChannelTypes.PRIVATE_THREAD, ChannelTypes.PUBLIC_THREAD].includes(channel?.type) && checkPermission(Permissions.VIEW_CHANNEL, currentUser, channel);
+    return [ChannelTypes.GUILD_TEXT, ChannelTypes.GUILD_VOICE, ChannelTypes.GUILD_STAGE_VOICE, ChannelTypes.GUILD_ANNOUNCEMENT, ChannelTypes.ANNOUNCEMENT_THREAD, ChannelTypes.PRIVATE_THREAD, ChannelTypes.PUBLIC_THREAD].includes(channel?.type) && checkPermission(Permissions.VIEW_CHANNEL, channel, currentUser);
 }
 
 const hiddenChannelCache = Object.values(getGuilds()).reduce((cache, currentGuild) => { 
