d4665188592541a4b67bc75402669703830cafb7
alethéia <34745171+xirreal@users.noreply.github.com>
2021-10-29 21:19:31 +0200

    trying some stuff

--- index.js
+++ index.js
@@ -58,9 +58,15 @@ const handleChannelChange = data => {
 }
 
 const isChannelVisible = channelId => {
-    const channel = getChannel(channelId);
-    if(!channel || !channelId || [ChannelTypes.DM, ChannelTypes.GROUP_DM].includes(channel?.type)) return true;
-    return [ChannelTypes.GUILD_TEXT, ChannelTypes.GUILD_VOICE, ChannelTypes.GUILD_STAGE_VOICE, ChannelTypes.GUILD_ANNOUNCEMENT, ChannelTypes.ANNOUNCEMENT_THREAD, ChannelTypes.PRIVATE_THREAD, ChannelTypes.PUBLIC_THREAD].includes(channel?.type) && checkPermission(Permissions.VIEW_CHANNEL, channel, currentUser);
+    try {
+    	const channel = getChannel(channelId);
+   	if(!channel || !channelId || [ChannelTypes.DM, ChannelTypes.GROUP_DM].includes(channel?.type)) return true;
+    	return [ChannelTypes.GUILD_TEXT, ChannelTypes.GUILD_VOICE, ChannelTypes.GUILD_STAGE_VOICE, ChannelTypes.GUILD_ANNOUNCEMENT, ChannelTypes.ANNOUNCEMENT_THREAD, ChannelTypes.PRIVATE_THREAD, ChannelTypes.PUBLIC_THREAD].includes(channel?.type) && checkPermission(Permissions.VIEW_CHANNEL, channel, currentUser);
+    }
+    catch(e){
+	console.log(e)
+	return true;
+    }
 }
 
 const hiddenChannelCache = Object.values(getGuilds()).reduce((cache, currentGuild) => { 
